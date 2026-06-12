f57041f76d0a0ee3277b43f4aaed4b97b2f5d280
Phi <49373597+phireiser@users.noreply.github.com>
2026-06-12 17:45:08 +0200

    improved container building script

--- run/container/build.sh
+++ run/container/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+set -euo pipefail
+
 source "$(dirname "$0")/src/path_setup.sh"
 
 podman build -f src/container/Dockerfile -t mol-spectro:latest .
-rm -f /tmp/mol-spectro.tar
-podman save mol-spectro:latest -o /tmp/mol-spectro.tar
-rm -f $SIF
-apptainer build $SIF docker-archive:///tmp/mol-spectro.tar
+TMP_DIR=$(mktemp -d)
+rm -f "$TMP_DIR"/mol-spectro.tar
+podman save mol-spectro:latest -o "$TMP_DIR"/mol-spectro.tar
+rm -f "$SIF"
+apptainer build "$SIF" docker-archive://"$TMP_DIR"/mol-spectro.tar
+rm -rf "$TMP_DIR"
